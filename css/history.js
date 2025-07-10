91f60298eaaa257ed303bef7a5982fccc4d1949e
sanghyeon-yun-ai <shyun4385@gmail.com>
2025-07-10 21:10:23 +0900

    Add files via upload

--- css/history.js
+++ css/history.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     let current = $(this).attr("data-tab");
     //.wrap .tab li에 클래스on 지워라.
     $(".wrap .tab li").removeClass("active");
-    $(".company_history").removeClass("active");
+    $(".game_history").removeClass("active");
     //클릭된나의 속성중 data-tab 을 가지고와서 클래스 on 을 붙여라.
     $(this).addClass("active");
     $("#" + current).addClass("active");
